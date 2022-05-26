5c1010fbf2df089a3bb9376764e93d0bbab25041
jooy3410 <jooy3410@naver.com>
2022-05-26 23:50:02 +0900

    k번째 수

---
+++ Algorithm/14.k번째 수.py
@@ -0,0 +1,7 @@
+T =int(input())
+for t in range(T):
+    n, s, e, k=map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a[s-1:e]
+    a.sort()
+    print("#%d $d" %(t, a[k-1]))
